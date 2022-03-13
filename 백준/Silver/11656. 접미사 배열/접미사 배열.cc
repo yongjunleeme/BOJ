73cf0dd00198dd248ac84b52d1c729b0544e505f
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-03-13 22:09:08 +0900

    [Silver IV] Title: 접미사 배열, Time: 4 ms, Memory: 2584 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.cc
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.cc
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
-vector <string> v;
+string st;
+vector<string> v;
 
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cin >> s;
-  int len = s.size();
-  int idx = 0;
-  while(idx < len){
-    string a;
-    for(int i = idx; i < len; i++) a += s[i];
-    idx++;
-    v.push_back(a);
+
+  cin >> st;
+  int stringlen = st.length();
+  for(int i = 0; i < stringlen; i++) {
+    string temp = st.substr(i);
+    v.push_back(temp);
   }
   sort(v.begin(), v.end());
-  for(auto i: v) cout << i << '\n';  
+  for(auto i : v) cout << i << '\n';
+  return 0;
 }

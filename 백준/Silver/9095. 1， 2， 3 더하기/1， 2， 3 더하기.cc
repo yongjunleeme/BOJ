0c2c9f129f90a6e6bad632b41e010b1bf3b87d83
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-03-15 13:55:19 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int arr[12];
-
-int solve(int n){
-    
-    if(n == 1) return 1;
-    if(n == 2) return 2;
-    if(n == 3) return 4;
-    
-    if(arr[n] != -1) return arr[n];
-    return arr[n] = solve(n - 1) + solve(n - 2) + solve(n - 3);
-}
-
+// d[i] = i를 1, 2, 3의 합으로 나타내는 방법의 수
+int d[20];
 int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int t;
-    cin >> t;
-    while(t--){
-        cin >> n;
-        fill(arr, arr + 12, -1);
-        cout << solve(n) << "\n";
-    }
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  d[1] = 1; d[2] = 2; d[3] = 4;
+  for(int i = 4; i < 11; i++)
+    d[i] = d[i-1] + d[i-2] + d[i-3];
+
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    cout << d[n] << '\n';
+  }
 }

3bc30bbedb9d779cd022676b87b23b8a45292caa
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-04-26 11:53:52 +0900

    [Gold V] Title: N-Queen, Time: 1624 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -9,26 +9,26 @@ int cnt = 0;
 
 // isused1 --> y       column (세로)
 // isused2 --> x+y     / (우측 상단 대각선)
-// isused3 --> x-y+n-1 \ (좌측 하단 대각선, n-1은 index 음수방지)
+// isused3 --> x-y+n-1 \ (우측 하단 대각선, n-1은 index 음수방지)
 
-void func(int cur){ //isused의 index와 func의 k를 x, y좌표로 삼는다.
-  if(cur == n){
+void func(int cur) { // cur번째 row에 말을 배치할 예정임
+  if (cur == n) { // N개를 놓는데 성공했다면
     cnt++;
     return;
   }
-  for(int i = 0; i < n; i++){
-    if(isused1[i] || isused2[cur+i] || isused3[cur-i+n-1]) continue;
+  for (int i = 0; i < n; i++) { // (cur, i)에 퀸을 놓을 예정
+    if (isused1[i] || isused2[i+cur] || isused3[cur-i+n-1]) // column이나 대각선 중에 문제가 있다면
+      continue;
     isused1[i] = 1;
-    isused2[cur+i] = 1;
+    isused2[i+cur] = 1;
     isused3[cur-i+n-1] = 1;
     func(cur+1);
     isused1[i] = 0;
-    isused2[cur+i] = 0;
+    isused2[i+cur] = 0;
     isused3[cur-i+n-1] = 0;
   }
 }
-
-int main(void){
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
   cin >> n;

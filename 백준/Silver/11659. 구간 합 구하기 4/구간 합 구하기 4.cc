2432151477096b541a6e7015736d4fe6e7291380
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-04-29 15:44:35 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -4,6 +4,15 @@ using namespace std;
 int n, m;
 int a[100004], d[100004];
 
+// 테이블
+// d[i] = 1부터 i까지의 합
+  
+// 점화식
+// d[i] = d[i-1] + a[i];
+
+// 초기값
+// d[0] = 0;
+
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -16,6 +25,6 @@ int main(void){
   while(m--){
     int i, j;
     cin >> i >> j;
-    cout << d[j] - d[i-1] << '\n';
+    cout << d[j] - d[i-1] << '\n'; // i-1은 범위 이전의 수를 뺀다.
   }
 }

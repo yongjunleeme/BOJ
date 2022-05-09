84ae2d37200bfa178e437b8542a5bdad45ded0ca
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-05-09 10:14:25 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 4012 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[505][505];
+int d[505][505];
+int n;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  
+  for(int i = 1; i <= n; i++)
+    for(int j = 1; j <= i; j++)
+      cin >> a[i][j];
+  
+  d[1][1] = a[1][1];
+  
+  for(int i = 2; i <= n; i++)
+    for(int j = 1; j <= i; j++)
+      d[i][j] = max(d[i-1][j], d[i-1][j-1]) + a[i][j];
+  
+  cout << *max_element(d[n]+1, d[n]+n+1);
+}

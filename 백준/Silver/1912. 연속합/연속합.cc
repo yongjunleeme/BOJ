6df94b78f442de80541daeff94e0bfd4b1e48998
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-05-11 11:35:55 +0900

    [Silver II] Title: 연속합, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -3,18 +3,14 @@ using namespace std;
 
 int n;
 int d[100005];
-int a[100005];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  // 연속합 최대값
   cin >> n;
   
-  for(int i = 1; i <= n; i++){
-    cin >> a[i];
-    d[i] = a[i];
-  }
+  for(int i = 1; i <= n; i++)
+    cin >> d[i];
   
   for(int i = 1; i <= n; i++)
     d[i] = max(d[i-1] + d[i], d[i]);

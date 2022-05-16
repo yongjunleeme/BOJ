345869641797d3d98094bd1c20abd2b43a83ca54
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-05-16 09:34:14 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll d[105];
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  d[1] = 1;
+  d[2] = 1;
+  d[3] = 1;
+
+  for(int i = 4; i <= 100; i++)
+    d[i] = d[i - 2] + d[i - 3];
+
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    cout << d[n] << '\n';
+  }
+}

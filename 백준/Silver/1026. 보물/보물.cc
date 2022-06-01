6a15405528ae8bdb4c4996b9b0ceb126c331c5b8
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-06-01 11:52:01 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[105];
+int b[105];
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  
+  for(int i = 0; i < n; i++) cin >> a[i];
+  for(int i = 0; i < n; i++) cin >> b[i];
+  sort(a, a+n);
+  sort(b, b+n);
+  
+  int s = 0;
+  for(int i = 0; i < n; i++)
+    s += a[i] * b[n-1-i];
+  
+  cout << s;
+}

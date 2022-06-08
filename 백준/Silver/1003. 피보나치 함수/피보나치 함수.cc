2a42a9ab771ac65ee663e26da21f94cc5a752e4d
yongjunleeme <48748376+yongjunleeme@users.noreply.github.com>
2022-06-08 11:59:50 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -12,19 +12,24 @@ int main(void) {
     fibo[i][0] = fibo[i - 1][0] + fibo[i - 2][0];
     fibo[i][1] = fibo[i - 1][1] + fibo[i - 2][1];
   }
+    
   int T;
   cin >> T;
+    
   while (T--) {
     int N;
     cin >> N;
-    cout << fibo[N][0] << " " << fibo[N][1] << endl;
+    cout << fibo[N][0] << " " << fibo[N][1] << '\n';
   }
 }
+
 /*
 1. 테이블 정하기
 fibo[i][k] = 숫자 i가 k(0 or 1)를 호출한 횟수
+
 2. 점화식 찾기
 fibo[i][k] = fibo[i - 1][k] + fibo[i - 2][k]
+
 3. 초기값 정하기
 fibo[0][0] = 1, fibo[1][1] = 1
 */

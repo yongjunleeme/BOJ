9db84cb2c84a84e0b31db5ea6e45385eab933dc9
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2022-07-01 14:21:39 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int N, a[10] = {}, ans = 1;
-    cin >> N;
-    
-    while (N)
-    {
-        a[N % 10]++;
-        N /= 10;
-    }
-    
-    for (int i = 0; i < 10; i++)
-    {
-        if (i == 6 || i == 9) continue;
-        ans = max(ans, a[i]);
-    }
-    
-    ans = max(ans, (a[6] + a[9] + 1) / 2);
-    cout << ans;
-    
+int a[10];
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n;
+  cin >> n;
+  
+  while(n > 0){
+    a[n%10]++;
+    n /= 10;
+  }
+  
+  int ans = 0;
+  for(int i = 0; i < 10; i++){
+    if(i == 6 || i == 9) continue;
+    ans = max(ans, a[i]);
+  }
+  
+  ans = max(ans, (a[6] + a[9] + 1) / 2);
+  cout << ans;
 }

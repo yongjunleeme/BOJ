fc9623515562a56d39f721859d9a5d0d361b6d6c
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2022-07-01 10:59:55 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int A, B, C;	
+	cin >> A >> B >> C;
+	int t = A * B * C;
+	int d[10] = {};
+	
+	while (t > 0)
+	{
+		d[t % 10]++;
+		t /= 10;
+	}
+	
+	for (int i = 0; i < 10; i++)
+		cout << d[i] << '\n';
+}

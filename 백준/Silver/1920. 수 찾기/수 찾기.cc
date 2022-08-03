b1b4169181e574e7a4b4b60ddf626d5a0c9a0860
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2022-08-03 17:54:18 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -5,40 +5,17 @@ using namespace std;
 int a[100005];
 int n;
 
-int binarySearch(int target)
-{
-    int st = 0;
-    int en = n - 1;
-    
-    while (st <= en)
-    {
-        int mid = (st + en) / 2;
-        if (a[mid] < target)
-            st = mid + 1;
-        else if (a[mid] > target)
-            en = mid - 1;
-        else
-            return 1;
-    }
-    
-    return 0;
-}
-
-int main(void)
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    sort(a, a + n);
-    
-    int m;
-    cin >> m;
-    while (m--)
-    {
-        int t;
-        cin >> t;
-        cout << binarySearch(t) << "\n";
-    }
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for(int i = 0; i < n; i++) cin >> a[i];
+  sort(a, a+n);
+  int m;
+  cin >> m;
+  while(m--){
+    int t;
+    cin >> t;
+    cout << binary_search(a, a+n, t) << '\n';
+  }
 }

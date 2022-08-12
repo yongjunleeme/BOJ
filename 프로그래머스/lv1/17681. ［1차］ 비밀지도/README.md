629d7d06d117fe09d83c2650d68774e5c66ca6d6
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2022-08-12 22:52:23 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.76 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=java) 
 
 ### 성능 요약
 
-메모리: 3.59 MB, 시간: 0.01 ms
+메모리: 87.3 MB, 시간: 2.76 ms
 
 ### 구분
 
--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,29 +1,21 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        for(int i = 0; i < n; i++){
-            answer[i] = i2str3(arr1[i] | arr2[i], n);
-        }        
-        return answer;
-    }
-    
-    public String i2str2(int x, int n){
-        String tmp = "";
-        for(int i = 0; i < n; i++){
-            if(x % 2 == 1) tmp += '#';
-            else tmp += ' ';
-            x /= 2;
-        }
+    String func(int x, int n){
         String ret = "";        
-        for(int i = 0; i < n; i++){
-            ret += tmp.charAt(n-1-i);
+        for(int i = n-1; i >= 0; i--){
+            if((x & (1 << i)) != 0) ret += "#";
+            else ret += " ";
         }
         return ret;
     }
     
-    public String i2str3(int x, int n){
-        String ret = Integer.toBinaryString(x);
-        while(ret.length() < n) ret = " " + ret;
-        return ret.replace('0', ' ').replace('1', '#');
-    }
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i = 0; i < n; i++){
+            answer[i] = func(arr1[i] | arr2[i], n);            
+        }  
+        
+        return answer;
+    }    
+    
 }

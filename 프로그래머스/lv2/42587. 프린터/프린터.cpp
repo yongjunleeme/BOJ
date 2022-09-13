f384d23375f0df1453aea03987f690d1f142f1e9
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2022-09-13 13:22:52 +0900

    [level 2] Title: 프린터, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    priority_queue <int> pq;
+    queue <pair<int, int>> q;
+    
+    for(int i = 0; i < priorities.size(); i++){
+        pq.push(priorities[i]);
+        q.push({priorities[i], i});
+    }
+    int cnt = 0;
+    
+    while(!q.empty()){
+        if(q.front().first == pq.top()){
+            cnt++;            
+            if(q.front().second == location) return cnt;
+            q.pop(); pq.pop();            
+        } else {
+            auto a = q.front();
+            q.push({a.first, a.second});
+            q.pop();
+        }
+    }
+    
+}

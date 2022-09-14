9b8f118808cc98c5bf56cad8b47e683b81bae137
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2022-09-14 15:39:19 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int idx = 0;
+    int cnt = 0;
+    queue <int> q;
+    int w = 0;
+    while(1) {
+        cnt++;
+        
+        if(q.size() == bridge_length){
+            w -= q.front();
+            q.pop();
+        }
+        
+        if(weight >= w + truck_weights[idx]){
+            if(idx == truck_weights.size() - 1){
+                cnt += bridge_length;
+                break;
+            }
+            
+            q.push(truck_weights[idx]);
+            w += truck_weights[idx];
+            idx++;            
+        } else {
+            q.push(0);
+        }                          
+    }
+    return cnt;
+    
+    
+    
+}

08eba899f5951989ad205b8ce9f5365183fd8f69
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2022-11-05 17:37:57 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
@@ -2,24 +2,18 @@
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    
-    vector <int> tmp; 
-    vector<int> ans;
-    int mx = 0;
-    
+    vector <int> tmp;
+    vector <int> ans;
+    stack <int> stk;
     for(int i = 0; i < progresses.size(); i++){
-        int a = (99 - progresses[i]) / speeds[i] + 1;   
-        tmp.push_back(a);
+        tmp.push_back((99 - progresses[i]) / speeds[i] + 1);
     }
-    
     for(int i = 0; i < tmp.size(); i++){
-        if(ans.empty() || tmp[i] > mx) ans.push_back(1);
+        if(ans.empty() || tmp[i] > stk.top()) ans.push_back(1);
         else ans.back()++;
         
-        mx = max(mx, tmp[i]);
+        if(stk.empty() || stk.top() < tmp[i]) stk.push(tmp[i]);
+        
     }
-    
-    
     return ans;
-    
 }

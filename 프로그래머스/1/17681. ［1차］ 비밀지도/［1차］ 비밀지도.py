0991b91f6c8a851368ca185655cfe49aa8934fd5
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2024-02-12 09:59:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,13 @@
+def solution(n, arr1, arr2):
+    # 벽 or, 공백 and
+    ans = []
+    for i in range(n):
+        ans.append(
+            bin(arr1[i] | arr2[i])[2:]
+                .zfill(n)
+                .replace("1", "#")
+                .replace("0", " ")
+        )        
+    
+    return ans
+        

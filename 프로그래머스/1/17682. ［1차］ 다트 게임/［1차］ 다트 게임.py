dc73f30bb00a863e45b02b42dcd26f365c3c144b
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2024-02-12 13:34:51 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,26 @@
+def solution(d):
+    ans = [0]
+    
+    for i in d:
+        if i == 'S':
+            ans[-1] **= 1
+            ans.append(0)
+        elif i == 'D':
+            ans[-1] **= 2
+            ans.append(0)
+        elif i == 'T':
+            ans[-1] **= 3
+            ans.append(0)
+        elif i == '*':
+            ans[-2] *= 2
+            if len(ans) > 2:
+                ans[-3] *= 2
+        elif i == '#':
+            ans[-2] *= -1
+        else:
+            ans[-1] = (ans[-1] * 10) + int(i)
+            
+    print(ans)
+    return sum(ans)
+            
+    

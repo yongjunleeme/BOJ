31b6e71a79da3710562d8a9feabccd8a8cab994f
yongjunlee <48748376+yongjunleeme@users.noreply.github.com>
2024-02-17 11:08:50 +0900

    [level 2] Title: [1차] 캐시, Time: 0.87 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -2,16 +2,13 @@ def solution(cacheSize, cities):
     from collections import deque
     q = deque(maxlen=cacheSize)
     ans = 0
-    for city in cities:
-        city = city.lower()
-        
-        if city in q:
-            q.remove(city)
-            q.append(city)
+    for i in cities:
+        i = i.lower()
+        if i in q:
+            q.remove(i)
+            q.append(i)
             ans += 1
         else:
-            q.append(city)
-            ans += 5
-        
-            
+            q.append(i)
+            ans += 5        
     return ans
